770197c605910045e7c4c7fc1b427ba640ce61a6
hvaleri0 <hector.valerio@gmail.com>
2024-06-13 16:57:04 -0400

    complete navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable react/jsx-no-target-blank */
 import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav, Button } from "react-bootstrap";
 
 // * Style
 import "./style.css";
@@ -11,15 +11,8 @@ import logo from "../../assets/Logo.png";
 
 function NavBar() {
   return (
-    <Navbar
-      collapseOnSelect
-      className="navBar"
-      expand="md"
-      fixed="top"
-      // sticky="top"
-    >
-      <Navbar.Toggle aria-controls="navbar-content" />
-      <Navbar.Brand href="#home">
+    <Navbar collapseOnSelect className="navBar" expand="md" fixed="top">
+      <Navbar.Brand href="#home" className="brand">
         <img
           src={logo}
           width="300"
@@ -27,21 +20,22 @@ function NavBar() {
           alt="Synergy Litigation Services logo"
         />
       </Navbar.Brand>
-
+      <Navbar.Toggle aria-controls="navbar-content" />
       <Navbar.Collapse id="navbar-content">
-        <Nav className="mr-auto">
+        <Nav className="ml-auto">
           <Nav.Link href="#home">Home</Nav.Link>
           <Nav.Link href="#services">Services</Nav.Link>
           <Nav.Link href="#contact">Contact Us</Nav.Link>
         </Nav>
-        <a
+        <Button
+          variant="outline-primary"
+          as="a"
           href="https://google.com"
-          className="download"
           target="_blank"
           rel="noreferrer noopener"
         >
           Access MySLS Client Portal
-        </a>
+        </Button>
       </Navbar.Collapse>
     </Navbar>
   );
