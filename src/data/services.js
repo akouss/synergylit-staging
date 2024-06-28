8fab902c66481eb86b851d7e15aa34157be2984d
hvaleri0 <hector.valerio@gmail.com>
2024-06-28 11:37:32 -0400

    Update Sevices description in Corosel

--- src/data/services.js
+++ src/data/services.js
@@ -21,32 +21,34 @@ const services = [
     id: "1",
     title: "Realtime",
     description:
-      'Realtime is the act of connecting directly to a court reporter during a deposition or in court to see the transcript of the testimony as it is being recorded "in real time." Think of it as live closed captioning with which you can interact. Realtime viewed on an iPad using iCVNet can be highlighted and searched. However, at the end of your deposition, it can be imported to CaseView Net on your PC where you can also mark issues as well as run reports.',
+      "Realtime allows you to connect directly to a court reporter during a deposition or court session to view and interact with the transcript live, similar to closed captioning. You can highlight and search the transcript on an iPad using iCVNet. After the deposition, the transcript can be imported to CaseView Net on your PC for further marking and reporting.",
   },
   {
     id: "2",
     title: "Digital/Video Depositions",
 
-    description:
-      "Since the beginning, we’ve offered accurate and professional court reporting services while staying within the parameters of necessary time constraints. We make deposition schedules work the way they need to work by using the latest, most customized and most reliable technology.\n\nWe provide the same quality services to firms of all sizes, regardless of what area of litigation in which they practice. As our history shows, we’ve done it all. From complex litigation to general depositions, sworn statements, arbitrations, hearings and meetings, we have the expertise to serve you.\n\nIn addition to standard video depositions, Synergy Litigation Services now offers Digital Depositions, which is a cost-saving alternative to traditional stenographic court reporting and video.\n\nRather than taking down testimony from hearing, the proceeding is recorded by a Certified Electronic Reporter. The deposition is digitally recorded using multi-track, broadcast quality video and audio equipment.\n\nFederal and Civil courts, across all 50 states, recognize and utilize digital depositions. We strictly follow all Rules of Civil Procedure as it relates to providing an official record.\n\nPackage Includes:\nCertified transcript\nVideo at no extra charge\nAt the end of the deposition the attorney will receive a DVD or thumb drive of the deposition.",
+    description: `Synergy Litigation Services offers professional court reporting tailored to meet tight schedules using advanced, reliable technology. We serve all sizes of firms across various litigation areas, with a history of handling everything from complex cases to general depositions, arbitrations, and meetings.\n
+    We now provide Digital Depositions, a cost-effective alternative to traditional stenographic reporting, using multi-track, broadcast-quality recording equipment. Recognized by federal and civil courts nationwide, these depositions comply fully with the Rules of Civil Procedure.\n
+    Our package includes a certified transcript and a complimentary video, delivered on a DVD or thumb drive at the deposition's conclusion.`,
   },
 
   {
     id: "3",
     title: "Mobile Videoconferencing",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    description: `Struggling to arrange depositions for witnesses in distant or remote locations? Synergy Litigation Services' Mobile Videoconferencing allows you to conduct face-to-face meetings safely and easily from your office, requiring only high-speed internet and a mobile device with a webcam.
+    Our service supports full videoconferencing on a range of devices including PC laptops, Android devices, MacBooks, iPads, tablets, iPhones, and smartphones with cameras, making it the most cost-effective solution for participating in depositions.`,
   },
   {
     id: "4",
     title: "Video Synchronization",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    description: `Synergy Litigation Services enables you to view both the deponent and their testimony in the video recording. We sync the deposition video with the transcript, allowing the testimony to display as it occurs in the recording. You can also search the video record for key testimony and import the video files into your trial presentation software`,
   },
   {
     id: "5",
     title: "Transcription Services",
-    description: "Nulla vitae elit libero, a pharetra augue mollis interdum.",
+    description: `Synergy Litigation Services offers a comprehensive range of transcription options. 
+    Our transcriptionists can convert various forms of recorded speech—audio, video, and digital recordings of interviews or proceedings—into hard copy or electronic text with speed and accuracy. 
+    Just contact us with your needs. Our formats include: Realtime, Minuscript, E-transcript, ASCII, DVD/CD, RealLegal Binder, Summation, Amicus`,
   },
 ];
 
